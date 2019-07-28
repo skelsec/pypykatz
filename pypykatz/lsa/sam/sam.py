b1119cdfd52654b09de28f0baf79dff89d1dc45c
skelsec <info@skelsec.com>
2019-07-28 22:15:30 +0200

    continuing registry stuff

--- pypykatz/lsa/sam/sam.py
+++ pypykatz/lsa/sam/sam.py
@@ -1,12 +1,204 @@
 
 import hashlib
-import aiowinreg
-from aiowinreg.hive import AIOWinRegHive
+import hmac
 from pypykatz.lsa.sam.structures import *
 from pypykatz.crypto.RC4 import RC4
-from pypykatz.crypto.aes import AESModeOfOperationCBC, Decrypter
+from pypykatz.crypto.aes import AESModeOfOperationCBC,AESModeOfOperationECB, Decrypter
 from pypykatz.crypto.des import *
 
+#####
+import aiowinreg
+from aiowinreg.hive import AIOWinRegHive
+
+
+#####
+from pypykatz.lsa.security.structures import *
+
+
+class SECURITY:
+	def __init__(self, security_hive, bootkey):
+		self.hive = security_hive
+		self.bootkey = bootkey
+		
+		self.dcc_iteration_count = 10240
+		self.lsa_secret_key_vista_type = True
+		
+		self.lsa_key = None
+		self.NKLM_key = None
+	
+	@staticmethod
+	def sha256_multi(key, value, rounds = 1000):
+		ctx = hashlib.sha256(key)
+		for i in range(rounds):
+			ctx.update(value)
+		return ctx.digest()
+		
+	def decrypt_lsa_key(self, data):
+		if self.lsa_secret_key_vista_type is True:
+			record = LSA_SECRET.from_bytes(data)
+			key = SECURITY.sha256_multi(self.bootkey, record.data[:32])
+			secret_dec = b''
+			cipher = AESModeOfOperationECB(key)
+			n = 16
+			for block in [record.data[32:][i:i+n] for i in range(0, len(record.data[32:]), n)]:  #terrible, terrible workaround
+				secret_dec += cipher.decrypt(block)
+			record = LSA_SECRET_BLOB.from_bytes(secret_dec)
+			self.lsa_key = record.secret[52:][:32]
+		
+		else:
+			ctx = hashlib.md5(self.bootkey)
+			for i in range(1000):
+				ctx.update(value[60:76])
+			
+			cipher = RC4.new(ctx.digest())
+			record = rc4.decrypt(value[12:60])
+			self.lsa_key = record[0x10:0x20]
+		
+		return self.lsa_key
+			
+		
+	def get_lsa_key(self):
+		value = self.hive.get_value('Policy\\PolEKList\\default')
+		if value is None:
+			value = self.hive.get_value('Policy\\PolSecretEncryptionKey\\default')
+			if not value:
+				return None
+			
+			self.lsa_secret_key_vista_type = False
+			
+		return self.decrypt_lsa_key(value[1])
+		
+		
+	def decrypt_secret(self, key, value):
+		pass
+		
+	def get_NKLM_key(self):
+		if self.lsa_key is None:
+			self.get_lsa_secret_key()
+			
+		value = self.hive.get_value('Policy\\Secrets\\NL$KM\\CurrVal\\default')
+		if value is None:
+			raise Exception('Could not find NL$KM in registry :(')
+			
+		if self.lsa_secret_key_vista_type is True:
+			self.NKLM_key = b''
+			record = LSA_SECRET.from_bytes(value[1])
+			key = SECURITY.sha256_multi(self.lsa_key, record.data[:32])
+			cipher = AESModeOfOperationECB(key)
+			n = 16
+			for block in [record.data[32:][i:i+n] for i in range(0, len(record.data[32:]), n)]:  #terrible, terrible workaround
+				self.NKLM_key += cipher.decrypt(block)
+			
+		else:
+			self.NKLM_key = self.decrypt_secret(self.lsa_key, value[1])
+			
+		return self.NKLM_key
+		
+	def __pad(self, data):
+		if (data & 0x3) > 0:
+			return data + (data & 0x3)
+		else:
+			return data
+		
+	def dump_dcc(self):
+		cache_reg = self.hive.find_key('Cache')
+		values = self.hive.list_values(cache_reg)
+		if values == []:
+			return
+			
+		if b'NL$Control' in values:
+			values.remove(b'NL$Control')
+			
+		if b'NL$IterationCount' in values:
+			values.remove(b'NL$IterationCount')
+			record = self.getValue('Cache\\NL$IterationCount')[1]
+			if record > 10240:
+				self.dcc_iteration_count = record & 0xfffffc00
+			else:
+				self.dcc_iteration_count = record * 1024
+				
+		
+		self.get_lsa_key()
+		self.get_NKLM_key()
+		
+		print(self.lsa_key)
+		print(self.NKLM_key)
+		
+		for value in values:
+			print('Checking value: %s' % value)
+			record_data = self.hive.get_value('Cache\\%s' % value.decode())[1]
+			record = NL_RECORD.from_bytes(record_data)
+			
+			if record.IV != b'\x00'*16:
+				if record.Flags & 1 == 1:
+					# Encrypted
+					if self.lsa_secret_key_vista_type is True:
+						plaintext = b''
+						cipher = AESModeOfOperationCBC(self.NKLM_key[16:32], iv = record.IV)
+						n = 16
+						for block in [record.EncryptedData[i:i+n] for i in range(0, len(record.EncryptedData), n)]:  #terrible, terrible workaround
+							if len(block) < 16:
+								block += b'\x00' * (16 - len(block))
+							plaintext += cipher.decrypt(block)
+							
+					else:
+						key = hmac.new(self.NKLM_key,record.IV).digest()
+						cipher = RC4(key)
+						plaintext = cipher.decrypt(record.EncryptedData)
+						
+				else:
+					# Plain! Until we figure out what this is, we skip it
+					#plainText = record['EncryptedData']
+					continue
+				
+				
+				dcc_hash = plaintext[:0x10]
+				blob = io.BytesIO(plaintext[0x48:])
+				username = blob.read(record.UserLength).decode('utf-16-le')
+				blob.seek(self.__pad(record.UserLength) + self.__pad(record.DomainNameLength))
+				domain = blob.read(record.DnsDomainNameLength).decode('utf-16-le')
+				
+				if self.lsa_secret_key_vista_type is True:
+					ans = "%s/%s:$DCC2$%s#%s#%s" % (domain, username, self.dcc_iteration_count, username, dcc_hash.hex())
+				else:
+					ans = "%s/%s:%s:%s" % (domain, username, dcc_hash.hex(), username)
+					
+				print(ans)
+				
+	def dump_secrets(self):
+		self.get_lsa_key()
+		self.get_NKLM_key()
+		
+		# Let's first see if there are cached entries
+		keys = self.hive.enum_key('Policy\\Secrets')
+		if keys is None:
+			# No entries
+			return
+			
+		if b'NL$Control' in keys:
+			keys.remove(b'NL$Control')
+		
+		for key_name in keys:
+			for vl in ['CurrVal', 'OldVal']:
+				key_path = 'Policy\\Secrets\\{}\\{}\\default'.format(key_name,vl)
+				print(key_path)
+				v = self.hive.get_value(key_path)
+				if v and v[1] != 0:
+					if self.lsa_secret_key_vista_type is True:
+						record = LSA_SECRET.from_bytes(v[1])
+						key = SECURITY.sha256_multi(self.lsa_key, record.data[:32])
+						secret_dec = b''
+						cipher = AESModeOfOperationECB(key)
+						n = 16
+						for block in [record.data[32:][i:i+n] for i in range(0, len(record.data[32:]), n)]:  #terrible, terrible workaround
+							secret_dec += cipher.decrypt(block)
+						record = LSA_SECRET_BLOB.from_bytes(secret_dec)
+						secret = record.secret
+					else:
+						raise NotImplementedError('TODO')
+				
+				print(secret)
+						
 
 class SYSTEM:
 	def __init__(self, system_hive):
@@ -38,11 +230,26 @@ class SYSTEM:
 		return self.bootkey
 		
 class SAMSecret:
-	def __init__(self):
-		self.username
-		self.rid
-		self.nt_hash
-		self.lm_hash
+	def __init__(self, username, rid, nt_hash, lm_hash):
+		self.username = username
+		self.rid = rid
+		self.nt_hash = nt_hash
+		self.lm_hash = lm_hash
+		
+	def to_dict(self):
+		return {
+			'username' : username,
+			'rid' : self.rid,
+			'nt_hash' : self.nt_hash,
+			'lm_hash' : self.lm_hash,
+		}
+		
+	def to_json(self):
+		return json.dumps(self.to_dict())
+	
+	def to_lopth(self):
+		return '%s:%s:%s:%s:::' % (self.username, self.rid, self.lm_hash.hex(), self.nt_hash.hex())
+		
 		
 
 class SAM:
@@ -50,6 +257,7 @@ class SAM:
 		self.hive = sam_hive
 		self.bootkey = bootkey
 		self.hashed_bootkey = None
+		self.secrets = []
 		
 	@staticmethod
 	def rid_to_key(rid):
@@ -123,7 +331,6 @@ class SAM:
 				continue
 			
 			uac_data = self.hive.get_value('SAM\\Domains\\Account\\Users\\%s\\V' % rid)[1]
-			input('uac_data')
 			uac = USER_ACCOUNT_V.from_bytes(uac_data)
 			
 			nthash = bytes.fromhex(NTDEFAULT)
@@ -142,10 +349,12 @@ class SAM:
 			elif uac.NT_hash and isinstance(uac.NT_hash, SAM_HASH):
 				if uac.NT_hash.hash != b'':
 					nthash = self.decrypt_hash(rid, uac.NT_hash, NTPASSWORD)
-					
-			print('LM: %s' % lmhash)
-			print('NT: %s' % nthash)
-		
+			
+			secret = SAMSecret(uac.name, int(rid,16), nthash, lmhash)
+			print(secret.to_lopth())
+			self.secrets.append(secret)
+			
+		return self.secrets
 		
 if __name__ == '__main__':
 	
@@ -159,5 +368,11 @@ if __name__ == '__main__':
 		hive = AIOWinRegHive(f)
 		sam = SAM(hive, bootkey)
 		sam.dump()
+		
+	with open('SECURITY.reg', 'rb') as f:
+		hive = AIOWinRegHive(f)
+		sam = SECURITY(hive, bootkey)
+		sam.dump_dcc()
+		sam.dump_secrets()
 		
 		
---
+++ pypykatz/lsa/security/structures.py
@@ -0,0 +1,167 @@
+
+import enum
+import io
+
+class LSA_SECRET_BLOB:
+	def __init__(self):
+		self.legnth = None
+		self.unk = None
+		self.secret = None
+		#self.remaining = None
+	
+	@staticmethod
+	def from_bytes(data):
+		return LSA_SECRET_BLOB.from_buffer(io.BytesIO(data))
+
+	@staticmethod
+	def from_buffer(buff):
+		sk = LSA_SECRET_BLOB()
+		sk.legnth = int.from_bytes(buff.read(4), 'little', signed = False)
+		sk.unk = buff.read(12)
+		sk.secret = buff.read(sk.legnth)
+		return sk
+		
+	def __str__(self):
+		t = '== LSA_SECRET_BLOB ==\r\n'
+		for k in self.__dict__:
+			if isinstance(self.__dict__[k], list):
+				for i, item in enumerate(self.__dict__[k]):
+					t += '   %s: %s: %s' % (k, i, str(item))
+			else:
+				t += '%s: %s \r\n' % (k, str(self.__dict__[k]))
+		return t
+		
+		
+class LSA_SECRET:
+	def __init__(self):
+		self.version = None
+		self.enc_key_id = None
+		self.enc_algo = None
+		self.flags = None
+		self.data = None
+	
+	@staticmethod
+	def from_bytes(data):
+		return LSA_SECRET.from_buffer(io.BytesIO(data))
+
+	@staticmethod
+	def from_buffer(buff):
+		sk = LSA_SECRET()
+		sk.version = int.from_bytes(buff.read(4), 'little', signed = False)
+		sk.enc_key_id = buff.read(16)
+		sk.enc_algo = int.from_bytes(buff.read(4), 'little', signed = False)
+		sk.flags = int.from_bytes(buff.read(4), 'little', signed = False)
+		sk.data = buff.read()
+		
+		return sk
+		
+	def __str__(self):
+		t = '== LSA_SECRET ==\r\n'
+		for k in self.__dict__:
+			if isinstance(self.__dict__[k], list):
+				for i, item in enumerate(self.__dict__[k]):
+					t += '   %s: %s: %s' % (k, i, str(item))
+			else:
+				t += '%s: %s \r\n' % (k, str(self.__dict__[k]))
+		return t
+		
+class LSA_SECRET_XP:
+	def __init__(self):
+		self.legnth = None
+		self.version = None
+		self.secret = None
+
+	@staticmethod
+	def from_bytes(data):
+		return LSA_SECRET_XP.from_buffer(io.BytesIO(data))
+
+	@staticmethod
+	def from_buffer(buff):
+		sk = LSA_SECRET_XP()		
+		sk.legnth = int.from_bytes(buff.read(4), 'little', signed = False)
+		sk.version = int.from_bytes(buff.read(4), 'little', signed = False)
+		sk.secret = buff.read(sk.legnth)
+		
+		return sk
+		
+	def __str__(self):
+		t = '== LSA_SECRET_XP ==\r\n'
+		for k in self.__dict__:
+			if isinstance(self.__dict__[k], list):
+				for i, item in enumerate(self.__dict__[k]):
+					t += '   %s: %s: %s' % (k, i, str(item))
+			else:
+				t += '%s: %s \r\n' % (k, str(self.__dict__[k]))
+		return t
+		
+		
+class NL_RECORD:
+	def __init__(self):
+		self.UserLength = None
+		self.DomainNameLength = None
+		self.EffectiveNameLength = None
+		self.FullNameLength = None
+		self.LogonScriptName = None
+		self.ProfilePathLength = None
+		self.HomeDirectoryLength = None
+		self.HomeDirectoryDriveLength = None
+		self.UserId = None
+		self.PrimaryGroupId = None
+		self.GroupCount = None
+		self.logonDomainNameLength = None
+		self.unk0 = None
+		self.LastWrite = None
+		self.Revision = None
+		self.SidCount = None
+		self.Flags = None
+		self.unk1 = None
+		self.LogonPackageLength = None
+		self.DnsDomainNameLength = None
+		self.UPN = None
+		self.IV = None
+		self.CH = None
+		self.EncryptedData = None
+		
+	@staticmethod
+	def from_bytes(data):
+		return NL_RECORD.from_buffer(io.BytesIO(data))
+
+	@staticmethod
+	def from_buffer(buff):
+		nl = NL_RECORD()		
+		nl.UserLength = int.from_bytes(buff.read(2), 'little', signed = False)
+		nl.DomainNameLength = int.from_bytes(buff.read(2), 'little', signed = False)
+		nl.EffectiveNameLength = int.from_bytes(buff.read(2), 'little', signed = False)
+		nl.FullNameLength = int.from_bytes(buff.read(2), 'little', signed = False)
+		nl.LogonScriptName = int.from_bytes(buff.read(2), 'little', signed = False)
+		nl.ProfilePathLength = int.from_bytes(buff.read(2), 'little', signed = False)
+		nl.HomeDirectoryLength = int.from_bytes(buff.read(2), 'little', signed = False)
+		nl.HomeDirectoryDriveLength = int.from_bytes(buff.read(2), 'little', signed = False)
+		nl.UserId = int.from_bytes(buff.read(4), 'little', signed = False)
+		nl.PrimaryGroupId = int.from_bytes(buff.read(4), 'little', signed = False)
+		nl.GroupCount = int.from_bytes(buff.read(4), 'little', signed = False)
+		nl.logonDomainNameLength = int.from_bytes(buff.read(2), 'little', signed = False)
+		nl.unk0 = int.from_bytes(buff.read(2), 'little', signed = False)
+		nl.LastWrite = int.from_bytes(buff.read(8), 'little', signed = False)
+		nl.Revision = int.from_bytes(buff.read(4), 'little', signed = False)
+		nl.SidCount = int.from_bytes(buff.read(4), 'little', signed = False)
+		nl.Flags = int.from_bytes(buff.read(4), 'little', signed = False)
+		nl.unk1 = int.from_bytes(buff.read(4), 'little', signed = False)
+		nl.LogonPackageLength = int.from_bytes(buff.read(4), 'little', signed = False)
+		nl.DnsDomainNameLength = int.from_bytes(buff.read(2), 'little', signed = False)
+		nl.UPN = int.from_bytes(buff.read(2), 'little', signed = False)
+		nl.IV = buff.read(16)
+		nl.CH = buff.read(16)
+		nl.EncryptedData = buff.read()
+		
+		return nl
+		
+	def __str__(self):
+		t = '== NL_RECORD ==\r\n'
+		for k in self.__dict__:
+			if isinstance(self.__dict__[k], list):
+				for i, item in enumerate(self.__dict__[k]):
+					t += '   %s: %s: %s' % (k, i, str(item))
+			else:
+				t += '%s: %s \r\n' % (k, str(self.__dict__[k]))
+		return t
