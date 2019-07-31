971b053e3c2b7d05e67da56d9cf78059aeda1f0f
skelsec <info@skelsec.com>
2019-08-01 00:06:41 +0200

    lsa dump progress

---
+++ pypykatz/dpapi/structures.py
@@ -0,0 +1,31 @@
+import io
+import enum
+
+
+class DPAPI_SYSTEM:
+	def __init__(self):
+		self.version = None
+		self.machine_key = None
+		self.user_key = None
+	
+	@staticmethod
+	def from_bytes(data):
+		return DPAPI_SYSTEM.from_buffer(io.BytesIO(data))
+
+	@staticmethod
+	def from_buffer(buff):
+		sk = DPAPI_SYSTEM()
+		sk.version = int.from_bytes(buff.read(4), 'little', signed = False) 
+		sk.machine_key = buff.read(20)
+		sk.user_key = buff.read(20)
+		return sk
+		
+	def __str__(self):
+		t = '== DPAPI_SYSTEM ==\r\n'
+		for k in self.__dict__:
+			if isinstance(self.__dict__[k], list):
+				for i, item in enumerate(self.__dict__[k]):
+					t += '   %s: %s: %s' % (k, i, str(item))
+			else:
+				t += '%s: %s \r\n' % (k, str(self.__dict__[k]))
+		return t
--- pypykatz/lsa/sam/sam.py
+++ pypykatz/lsa/sam/sam.py
@@ -13,7 +13,142 @@ from aiowinreg.hive import AIOWinRegHive
 
 #####
 from pypykatz.lsa.security.structures import *
+from pypykatz.dpapi.structures import DPAPI_SYSTEM
+from pypykatz.commons.common import hexdump
 
+class LSASecret:
+	def __init__(self,key_name, raw_secret, history = False):
+		self.raw_secret = raw_secret
+		self.key_name = key_name
+		self.history = history
+	
+	@staticmethod
+	def process(key_name, raw_secret, history = False):
+		kn = key_name.upper()
+		print(key_name)
+		if len(raw_secret) == 0:
+			return
+		if raw_secret.startswith(b'\x00\x00'):
+			return
+		
+		if kn.startswith('_SC_'):
+			lss = LSASecretService(kn, raw_secret, history)
+			lss.process_secret()
+			
+		elif kn.startswith('DEFAULTPASSWORD'):
+			lss = LSASecretDefaultPassword(kn, raw_secret, history)
+			lss.process_secret()
+			
+		elif kn.startswith('ASPNET_WP_PASSWORD'):
+			lss = LSASecretASPNET(kn, raw_secret, history)
+			lss.process_secret()
+			
+		elif kn.startswith('DPAPI_SYSTEM'):
+			lss = LSASecretDPAPI(kn, raw_secret, history)
+			lss.process_secret()
+			
+		elif kn.startswith('$MACHINE.ACC'):
+			lss = LSASecretMachineAccount(kn, raw_secret, history)
+			lss.process_secret()
+		
+		else:
+			lss = LSASecret(kn, raw_secret, history)
+			
+		return lss
+		
+	def __str__(self):
+		return '=== LSASecret %s ===\r\n' % self.key_name + '\r\nHistory: %s' % self.history + '\r\nSecret: \r\n' + hexdump(self.raw_secret)
+		
+		
+class LSASecretService(LSASecret):
+	def __init__(self, key_name, raw_secret, history):
+		LSASecret.__init__(self, key_name, raw_secret, history)
+		self.service = None
+		self.username = None
+		self.secret = None
+		
+	def process_secret(self):
+		try:
+			self.secret = self.raw_secret.decode('utf-16-le')
+		except:
+			pass
+		else:
+			#here you may implement a mechanism to fetch the service user's name
+			#TODO
+			self.service = self.key_name
+			self.username = 'UNKNOWN'
+			
+	def __str__(self):
+		return '=== LSA Service User Secret ===\r\nHistory: %s\r\nService name: %s \r\nUsername: %s' % (self.history, self.service, self.username) + '\r\n' + hexdump(self.secret)
+		
+class LSASecretDefaultPassword(LSASecret):
+	def __init__(self, key_name, raw_secret, history):
+		LSASecret.__init__(self, key_name, raw_secret, history)
+		self.username = None
+		self.secret = None
+		
+	def process_secret(self):
+		try:
+			self.secret = self.raw_secret.decode('utf-16-le')
+		except:
+			pass
+		else:
+			#here you may implement a mechanism to fetch the default logon user
+			#TODO
+			self.username = 'UNKNOWN'
+			
+	def __str__(self):
+		return '=== LSA Default Password ===\r\nHistory: %s\r\nUsername: %s\r\nPassword: %s' % (self.history, self.username,self.secret)
+		
+class LSASecretASPNET(LSASecret):
+	def __init__(self, key_name, raw_secret, history):
+		LSASecret.__init__(self, key_name, raw_secret, history)
+		self.username = 'ASPNET'
+		self.secret = None
+		
+	def process_secret(self):
+		try:
+			self.secret = self.raw_secret.decode('utf-16-le')
+		except:
+			pass
+	
+	def __str__(self):
+		return '=== LSA ASPNET Password ===\r\nHistory: %s\r\nUsername: %s\r\nPassword: %s' % (self.history, self.username,self.secret)
+		
+
+class LSASecretMachineAccount(LSASecret):
+	def __init__(self, key_name, raw_secret, history):
+		LSASecret.__init__(self, key_name, raw_secret, history)
+		self.username = None
+		self.secret = None
+		self.kerberos_password = None
+	
+	def process_secret(self):
+		#only the NT hash is calculated here
+		ctx = hashlib.new('md4')
+		ctx.update(self.raw_secret)
+		self.secret = ctx.digest()
+		
+		#thx dirkjan
+		self.kerberos_password = self.raw_secret.decode('utf-16-le', 'replace').encode('utf-8', 'replace')
+		
+	def __str__(self):
+		return '=== LSA Machine account password ===\r\nHistory: %s\r\nNT: %s\r\nPassword(hex): %s\r\nKerberos password(hex): %s' % (self.history, self.secret.hex(), self.raw_secret.hex(), self.kerberos_password.hex())
+	
+		
+class LSASecretDPAPI(LSASecret):
+	def __init__(self, key_name, raw_secret, history):
+		LSASecret.__init__(self, key_name, raw_secret, history)
+		self.machine_key = None
+		self.user_key = None
+
+	def process_secret(self):
+		ds = DPAPI_SYSTEM.from_bytes(self.raw_secret)
+		self.machine_key = ds.machine_key
+		self.user_key = ds.user_key
+		
+	def __str__(self):
+		return '=== LSA DPAPI secret ===\r\nHistory: %s\r\nMachine key (hex): %s\r\nUser key(hex): %s' % (self.history, self.machine_key.hex(), self.user_key.hex())
 
 class SECURITY:
 	def __init__(self, security_hive, bootkey):
@@ -70,7 +205,22 @@ class SECURITY:
 		
 		
 	def decrypt_secret(self, key, value):
-		pass
+		dec_blob = b''
+		enc_size = int.from_bytes(value[:4], 'little', signed = False)
+		value = value[4:]
+		t_key = key
+		for _ in range(0, len(value), 8):
+			enc_blob = value[:8]
+			des_key = expand_DES_key(t_key[:7])
+			ctx = des(des_key)
+			dec_blob += ctx.decrypt(enc_blob)
+			t_key = t_key[7:]
+			value = value[8:]
+			if len(t_key) < 7:
+				t_key = key[len(t_key) : ]
+			
+		secret = LSA_SECRET_XP.from_bytes(dec_blob)
+		return secret.secret
 		
 	def get_NKLM_key(self):
 		if self.lsa_key is None:
@@ -193,12 +343,13 @@ class SECURITY:
 						for block in [record.data[32:][i:i+n] for i in range(0, len(record.data[32:]), n)]:  #terrible, terrible workaround
 							secret_dec += cipher.decrypt(block)
 						record = LSA_SECRET_BLOB.from_bytes(secret_dec)
-						secret = record.secret
-					else:
-						raise NotImplementedError('TODO')
-				
-				print(secret)
+						dec_blob = record.secret
 						
+					else:
+						dec_blob = self.decrypt_secret(self.lsa_key, v[1])
+						
+				secret = LSASecret.process(key_name, dec_blob, vl == 'OldVal')
+				print(str(secret))
 
 class SYSTEM:
 	def __init__(self, system_hive):
