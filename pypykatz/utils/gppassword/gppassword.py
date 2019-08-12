9b460a4e63b29ba33ecbb114785afa775e72baab
skelsec <info@skelsec.com>
2019-08-12 15:38:39 +0200

    Update gppassword.py

--- pypykatz/utils/gppassword/gppassword.py
+++ pypykatz/utils/gppassword/gppassword.py
@@ -2,6 +2,7 @@
 import base64
 from pypykatz.crypto.unified.aes import AES
 from pypykatz.crypto.unified.common import SYMMETRIC_MODE
+from pypykatz.crypto.unified.pkcs7 import unpad as pkcs7_unpad
 
 AES_KEY = b'\x4e\x99\x06\xe8\xfc\xb6\x6c\xc9\xfa\xf4\x93\x10\x62\x0f\xfe\xe8\xf4\x96\xe8\x06\xcc\x05\x79\x90\x20\x9b\x09\xa4\x33\xb6\x6c\x1b'
 AES_IV = b'\x00'*16
@@ -20,8 +21,8 @@ class GPPassword:
 			
 		pw_enc = base64.b64decode(data)
 		
-		ctx = AES(AES_KEY, iv = AES_IV)
-		pw_dec  = ctx.decrypt(pw_enc)
+		ctx = AES(AES_KEY, mode = SYMMETRIC_MODE.CBC, iv = AES_IV)
+		pw_dec  = pkcs7_unpad(ctx.decrypt(pw_enc))
 		
 		return pw_dec.decode('utf-16-le')
 	
