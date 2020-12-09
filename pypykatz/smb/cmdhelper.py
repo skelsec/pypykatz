690c1f1bd1b84ddf9bac611969b0280e6fa4abac
skelsec <info@skelsec.com>
2020-12-09 01:44:30 +0100

    smb epilog

--- pypykatz/smb/cmdhelper.py
+++ pypykatz/smb/cmdhelper.py
@@ -23,6 +23,7 @@ class SMBCMDArgs:
 		self.no_interactive = False
 		self.commands = ['login', 'i']
 
+smb_live_epilog = 'FOR AVAILABLE SUBCOMMANDS TYPE "... smb help" insted of "-h" '
 class SMBCMDHelper:
 	def __init__(self):
 		self.live_keywords = ['smb']
@@ -34,10 +35,11 @@ class SMBCMDHelper:
 		group.add_argument('url', help="SMB connection string")
 		group.add_argument('commands', nargs='*', help="!OPTIONAL! Takes a series of commands which will be executed until error encountered. If the command is 'i' is encountered during execution it drops back to interactive shell.")
 		
-		live_group = live_parser.add_parser('smb', help='SMB (live) client. Use "help" instead of "-h" to get the available subcommands ')
+		live_group = live_parser.add_parser('smb', help='SMB (live) client. Use "help" instead of "-h" to get the available subcommands', epilog=smb_live_epilog)
 		live_group.add_argument('--authmethod', choices=['ntlm', 'kerberos'], default = 'ntlm', help= 'Authentication method to use during login')
 		live_group.add_argument('--protocol-version', choices=['2', '3'], default = '2', help= 'SMB protocol version. SMB1 is not supported.')
 		live_group.add_argument('-v', '--verbose', action='count', default=0, help='Verbosity, can be stacked')
+		live_group.add_argument('host', help='Target host to connect to')
 		live_group.add_argument('commands', nargs='*', help="!OPTIONAL! Takes a series of commands which will be executed until error encountered. If the command is 'i' is encountered during execution it drops back to interactive shell.")
 		
 	def execute(self, args):
@@ -53,9 +55,9 @@ class SMBCMDHelper:
 		from winacl.functions.highlevel import get_logon_info
 		info = get_logon_info()
 		la = SMBCMDArgs()
-		la.smb_url = 'smb%s+sspi-%s://%s\\%s@%s' % (args.protocol_version, args.authmethod, info['domain'], info['username'], info['logonserver'])
+		la.smb_url = 'smb%s+sspi-%s://%s\\%s@%s' % (args.protocol_version, args.authmethod, info['domain'], info['username'], args.host)
 		la.verbose = args.verbose
-		input(la.smb_url)
+		print(la.smb_url)
 
 		if args.commands is not None and len(args.commands) > 0:
 			la.commands = []
