7e7f13cdacd927589b2414edf5fa290edb75701a
skelsec <info@skelsec.com>
2021-03-15 23:11:29 +0100

    smb version bump

--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ setup(
 		'aiowinreg>=0.0.4',
 		'msldap>=0.3.27',
 		'winacl>=0.1.1',
-		'aiosmb>=0.2.35',
+		'aiosmb>=0.2.36',
 	],
 	
 	# No more conveinent .exe entry point thanks to some idiot who 
