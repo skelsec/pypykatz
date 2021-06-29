2b8a66e106718350292695d843ad83db3346a07c
garanews <puntogtg@tiscali.it>
2021-06-29 16:33:56 +0200

    Update __init__.py
    fix new volatility3 syntax

--- pypykatz/commons/readers/volatility3/__init__.py
+++ pypykatz/commons/readers/volatility3/__init__.py
@@ -1,11 +1,11 @@
 import logging
 from typing import List
 
-from volatility.framework import interfaces, constants, exceptions, symbols
-from volatility.framework import renderers
-from volatility.framework.configuration import requirements
-from volatility.framework.objects import utility
-from volatility.framework.symbols import intermed
-from volatility.framework.symbols.windows import extensions
-from volatility.framework.layers.scanners import MultiStringScanner
-from volatility.plugins.windows import pslist, vadinfo
+from volatility3.framework import interfaces, constants, exceptions, symbols
+from volatility3.framework import renderers
+from volatility3.framework.configuration import requirements
+from volatility3.framework.objects import utility
+from volatility3.framework.symbols import intermed
+from volatility3.framework.symbols.windows import extensions
+from volatility3.framework.layers.scanners import MultiStringScanner
+from volatility3.plugins.windows import pslist, vadinfo
